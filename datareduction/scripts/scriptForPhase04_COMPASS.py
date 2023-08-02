d15a0ea16954d5019846bae2f53059a982e9fbae
Adele Plunkett <aplunket@nrao.edu>
2023-08-02 16:49:12 -0400

    adding phase04 script v2

--- datareduction/scripts/scriptForPhase04_COMPASS.py
+++ datareduction/scripts/scriptForPhase04_COMPASS.py
@@ -2,6 +2,7 @@
 ## We want to restart TCLEAN where ph03 stopped
 ## Manual editing will be needed, and will be marked 
 ## You can run it like: /opt/local/bin/mpicasa -n 8 /opt/local/bin/casa-alma --pipeline -c scriptForPhase04_COMPASS.py ???
+## casa-alma --pipeline -c scriptForPhase04_COMPASS.py
 
 import shutil
 import os
@@ -40,6 +41,7 @@ def setup_to_copy():
 ####################
 
 if doSetup: 
+    print('doing setup'
     setup_to_copy()
 
 os.chdir('working_reprocess_ph04/')
@@ -63,18 +65,20 @@ os.chdir('working_reprocess_ph04/')
 
 newthreshold='0.0038Jy'
 
-tclean(vis=visname, field='BHR71-IRS1',
+#### EDIT/CHECK ALL 4 FOLLOWING TCLEAN COMMANDS
+
+tclean(vis=visname, field='BHR71-IRS1', ## EDIT THIS LINE with vis=visname
        spw=['25'],
-       antenna=['0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41&'],
-       scan=['8,12,16,20,24,28'], intent='OBSERVE_TARGET#ON_SOURCE',
+       antenna=['0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41&'], ## CHECK THIS LINE
+       scan=['8,12,16,20,24,28'], intent='OBSERVE_TARGET#ON_SOURCE', ## CHECK THIS LINE
        datacolumn='data',
-       imagename='oussid.s10_0.BHR71-IRS1_sci.spw25.cube.I.iter2', imsize=[500,
-       500], cell=['0.074arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3837,
-       start='279.0475460151GHz', width='0.2441427MHz', outframe='LSRK',
+       imagename='oussid.s10_0.BHR71-IRS1_sci.spw25.cube.I.iter2', imsize=[500, ## EDIT THIS LINE with correct SPW
+       500], cell=['0.064arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3837,
+       start='279.0475460151GHz', width='0.2441427MHz', outframe='LSRK', ## EDIT THIS LINE with correct start=''
        perchanweightdensity=True, gridder='standard', mosweight=False,
        usepointing=False, pblimit=0.2, deconvolver='hogbom', restoration=True,
        restoringbeam='common', pbcor=True, weighting='briggsbwtaper',
-       robust=0.5, npixels=0, niter=2000000, threshold=newthreshold, nsigma=0.0,
+       robust=0.5, npixels=0, niter=1000000, threshold=newthreshold, nsigma=0.0, ## EDIT THIS LINE to use threshold=newthreshold
        interactive=0, usemask='auto-multithresh', sidelobethreshold=2.0,
        noisethreshold=5.0, lownoisethreshold=1.5, negativethreshold=7.0,
        minbeamfrac=0.3, growiterations=50, dogrowprune=True,
@@ -87,7 +91,7 @@ tclean(vis=visname, field='BHR71-IRS1',
        scan=['8,12,16,20,24,28'], intent='OBSERVE_TARGET#ON_SOURCE',
        datacolumn='data',
        imagename='oussid.s10_0.BHR71-IRS1_sci.spw27.cube.I.iter2', imsize=[500,
-       500], cell=['0.074arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3837,
+       500], cell=['0.064arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3837,
        start='279.9550542895GHz', width='0.2441427MHz', outframe='LSRK',
        perchanweightdensity=True, gridder='standard', mosweight=False,
        usepointing=False, pblimit=0.2, deconvolver='hogbom', restoration=True,
@@ -105,7 +109,7 @@ tclean(vis=visname, field='BHR71-IRS1',
        scan=['8,12,16,20,24,28'], intent='OBSERVE_TARGET#ON_SOURCE',
        datacolumn='data',
        imagename='oussid.s10_0.BHR71-IRS1_sci.spw29.cube.I.iter2', imsize=[500,
-       500], cell=['0.074arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3838,
+       500], cell=['0.064arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3838,
        start='280.8625898094GHz', width='0.2441427MHz', outframe='LSRK',
        perchanweightdensity=True, gridder='standard', mosweight=False,
        usepointing=False, pblimit=0.2, deconvolver='hogbom', restoration=True,
@@ -123,7 +127,7 @@ tclean(vis=visname, field='BHR71-IRS1',
        scan=['8,12,16,20,24,28'], intent='OBSERVE_TARGET#ON_SOURCE',
        datacolumn='data',
        imagename='oussid.s10_0.BHR71-IRS1_sci.spw31.cube.I.iter2', imsize=[500,
-       500], cell=['0.074arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3838,
+       500], cell=['0.064arcsec'], phasecenter='ICRS 12:01:36.4988 -065.08.49.381', stokes='I', specmode='cube', nchan=3838,
        start='281.7700748225GHz', width='0.2441428MHz', outframe='LSRK',
        perchanweightdensity=True, gridder='standard', mosweight=False,
        usepointing=False, pblimit=0.2, deconvolver='hogbom', restoration=True,
