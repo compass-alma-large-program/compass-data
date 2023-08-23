23dc196f8d7ff2e241c51d013d15a810adc51b2a
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2023-08-23 17:01:22 +0200

    Export BHR71 F setup data to UVFITS

---
+++ datareduction/scripts/imager/bhr71/export_f.py
@@ -0,0 +1,10 @@
+"""
+Export BHR71 F setup data to UVFITS
+
+"""
+
+import sys
+sys.path.append("../lib")
+from export import export
+
+export(uid = "uid___A002_X101c3b2_Xc445", field = "BHR71-IRS1", basename = "bhr71-f")
