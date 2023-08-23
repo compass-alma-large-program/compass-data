579f61fa0d6aead9b92220cd2e258b96769527de
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2023-08-23 16:46:08 +0200

    Move the factorized scripts to lib/

--- datareduction/scripts/imager/bhr71/export_a.py
+++ datareduction/scripts/imager/bhr71/export_a.py
@@ -4,7 +4,7 @@ Export BHR71 A setup data to UVFITS
 """
 
 import sys
-sys.path.append("../")
+sys.path.append("../lib")
 from export import export
 
 export(uid = "uid___A002_X101c3b2_Xbcf0", field = "BHR71-IRS1", basename = "bhr71-a")
