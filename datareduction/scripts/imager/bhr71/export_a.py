f02fd0c71b0e43d1fd7b871904f8a3c484d20725
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2023-08-23 15:33:14 +0200

    Factorize the export script

--- datareduction/scripts/imager/bhr71/export_a.py
+++ datareduction/scripts/imager/bhr71/export_a.py
@@ -1,83 +1,10 @@
 """
-Export the MS data to UVFITS
+Export BHR71 A setup data to UVFITS
 
 """
 
-import os
-import glob
-import shutil
-import tarfile
+import sys
+sys.path.append("../")
+from export import export
 
-basename = "bhr71-a"
-uid = "uid___A002_X101c3b2_Xbcf0"
-field="BHR71-IRS1"
-spws = [25, 27, 29, 31]
-datadir = "../../../../data/calibrated/"
-inputdir = os.path.realpath(datadir + "/ms")
-outputdir = os.path.realpath(datadir + "/uvfits")
-
-__rethrow_casa_exceptions = True
-
-os.chdir(inputdir)
-
-# Untar the file
-
-with tarfile.open(uid + ".ms.split.cal.tar") as tar:
-    tar.extractall()
-
-for spw in spws:
-    name = basename + "-spw%i" % spw
-
-    # Extract the data
-
-    shutil.rmtree(name + ".ms", ignore_errors=True)
-    split(
-        vis=uid + ".ms.split.cal",
-        outputvis=name + ".ms",
-        datacolumn="data",
-        intent="OBSERVE_TARGET#ON_SOURCE",
-        field=field,
-        spw="%i" % spw,
-        keepflags=False,
-    )
-
-    # Convert the velocity frame to LSR
-
-    shutil.rmtree(name + "-cvel.ms", ignore_errors=True)
-    cvel2(
-        vis=name + ".ms",
-        outputvis=name + "-cvel.ms",
-        outframe="LSRK",
-    )
-
-    # Export the MS to UVFITS
-    
-    exportuvfits(
-        vis=name + "-cvel.ms",
-        fitsfile=name + ".uvfits",
-        datacolumn="data",
-        multisource=False,
-        overwrite=True,
-    )
-
-    # Move the file to the output directory
-
-    try:
-        os.remove(outputdir + "/" + name + ".uvfits")
-    except OSError:
-        pass
-    shutil.move(name + ".uvfits", outputdir)
-
-    # Remove intermediate files
-
-    shutil.rmtree(name + ".ms", ignore_errors=True)
-    shutil.rmtree(name + "-cvel.ms", ignore_errors=True)
-
-# Remove CASA logs
-
-for f in glob.glob("*.last"):
-    os.remove(f)
-
-# Remove the untarred directory
-
-shutil.rmtree(uid + ".ms.split.cal", ignore_errors=True)
+export(uid = "uid___A002_X101c3b2_Xbcf0", field = "BHR71-IRS1", basename = "bhr71-a")
