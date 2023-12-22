461909f2da196e3dd2c03dfff4ae86f01902ba02
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2023-12-22 19:28:36 +0100

    Export HOPS-108 setup 8 data to UVFITS

---
+++ datareduction/scripts/imager/hops108/export_set8.py
@@ -0,0 +1,18 @@
+"""
+Export HOPS-108 setup 8 data to UVFITS
+
+"""
+
+import sys
+
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=[
+        "uid___A002_X101e5ab_X316b",
+        "uid___A002_X1020da2_X340c",
+    ],
+    field="HOPS-108",
+    basename="hops108-set8",
+)
