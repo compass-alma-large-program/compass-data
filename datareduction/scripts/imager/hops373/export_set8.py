ac4bd6aaf443df886c7161314c2577f5ca133046
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2023-12-24 09:50:41 +0100

    Export HOPS-373 setup 8 data to UVFITS

---
+++ datareduction/scripts/imager/hops373/export_set8.py
@@ -0,0 +1,18 @@
+"""
+Export HOPS-373 setup 8 data to UVFITS
+
+"""
+
+import sys
+
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=[
+        "uid___A002_X101e5ab_X316b",
+        "uid___A002_X1020da2_X340c",
+    ],
+    field="HOPS-373",
+    basename="hops373-set8",
+)
