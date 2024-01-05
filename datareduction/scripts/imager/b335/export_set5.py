00448c325c5c874aa85892aa8d8f4c49dcacd4ae
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-01-05 12:36:47 +0100

    Export B335 setup 5 data to UVFITS

---
+++ datareduction/scripts/imager/b335/export_set5.py
@@ -0,0 +1,10 @@
+"""
+Export B335 setting 5 data to UVFITS
+
+"""
+
+import sys
+sys.path.append("../lib")
+from export import export
+
+export(uid = "uid___A002_X1035744_Xffaa", field = "B335", basename = "b335-set5")
