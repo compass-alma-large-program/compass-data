13a4aa31b0397d48bef2de8dcb5ac0da6e785c4b
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-02-06 14:16:42 +0100

    Export IRAS4A setting 2 data to UVFITS

---
+++ datareduction/scripts/imager/iras4a/export_set2.py
@@ -0,0 +1,15 @@
+"""
+Export IRAS4A setting 2 data to UVFITS
+
+"""
+
+import sys
+
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=["uid___A002_X1119e64_X340e", "uid___A002_X1119e64_X39af"],
+    field="NGC1333_IRAS4A",
+    basename="iras4a-set2",
+)
