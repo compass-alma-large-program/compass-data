cebce090ad056791e83971635654fde7c398cf38
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-03-11 09:44:02 +0100

    Export V883_Ori setting 5 data to UVFITS

---
+++ datareduction/scripts/imager/v883ori/export_set5.py
@@ -0,0 +1,19 @@
+"""
+Export V883_Ori setting 5 data to UVFITS
+
+"""
+
+import sys
+
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=[
+        "uid___A002_X1038a19_X2369",
+        "uid___A002_X112077c_X14637",
+        "uid___A002_X112077c_X1cd7c",
+    ],
+    field="V883_Ori",
+    basename="v883ori-set5",
+)
