741bc3644c3c35c086e4093a38a3567f0024db95
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-05-31 14:05:25 +0200

    Export Serp-11 setting 6 data to UVFITS

---
+++ datareduction/scripts/imager/serp11/export_set6.py
@@ -0,0 +1,14 @@
+"""
+Export Serp-11 setting 6 data to UVFITS
+
+"""
+
+import sys
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=["uid___A002_X1175218_X4443", "uid___A002_X1175218_X3e66"],
+    field="Ser_emb_11_W",
+    basename="serp11-set6",
+)
