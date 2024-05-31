65dcf2fb9a3408c1c82b66998a2a0f34fb072762
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-05-31 14:30:50 +0200

    Export SerpS-18 setting 7 data to UVFITS

---
+++ datareduction/scripts/imager/serps18/export_set7.py
@@ -0,0 +1,14 @@
+"""
+Export SerpS-18 setting 7 data to UVFITS
+
+"""
+
+import sys
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=["uid___A002_X1173aab_Xa09c", "uid___A002_X1173aab_Xa728"],
+    field="SerpS-MM18a",
+    basename="serps18-set7",
+)
