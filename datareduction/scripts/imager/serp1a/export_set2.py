c927653fa296e61f2a7f7369f76046be96640802
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-06-26 11:47:53 +0200

    Export Serp-1a setting 2 data to UVFITS

---
+++ datareduction/scripts/imager/serp1a/export_set2.py
@@ -0,0 +1,14 @@
+"""
+Export Serp-1a setting 2 data to UVFITS
+
+"""
+
+import sys
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=["uid___A002_X117bb05_X8747", "uid___A002_X117bb05_X8142"],
+    field="Serp_SMM1-a",
+    basename="serp1a-set2",
+)
