706e13a48abc5fc21a7863ad7b83ce4282da4392
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-08-29 11:57:25 +0200

    Export SVS13 setting 7 data to UVFITS

---
+++ datareduction/scripts/imager/svs13/export_set7.py
@@ -0,0 +1,15 @@
+"""
+Export SVS13 setting 7 data to UVFITS
+
+"""
+
+import sys
+
+sys.path.append("../lib")
+from export import export
+
+export(
+    uid=["uid___A002_X1196e8b_X5023", "uid___A002_X1196e8b_X52bc"],
+    field="SVS13-A",
+    basename="svs13-set7",
+)
