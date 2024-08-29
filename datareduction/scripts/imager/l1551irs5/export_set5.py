0ee3a9431e3eaa6a3b9ae8384691473dbb7e31f7
Sébastien Maret <sebastien.maret@univ-grenoble-alpes.fr>
2024-08-29 11:39:13 +0200

    Export L1551-IRS5 setting 5 data to UVFITS

---
+++ datareduction/scripts/imager/l1551irs5/export_set5.py
@@ -0,0 +1,11 @@
+"""
+Export L1551-IRS5 setting 5 data to UVFITS
+
+"""
+
+import sys
+
+sys.path.append("../lib")
+from export import export
+
+export(uid="uid___A002_X1198c0c_Xc6f1", field="L1551_IRS_5", basename="l1551irs5-set5")
